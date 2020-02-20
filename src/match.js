3665ec81505311ffb431445a89c1671bbd452e59
Zack Shinkar <zack@havenlife.com>
2020-02-20 15:21:39 -0500

    Create channels

--- src/match.js
+++ src/match.js
@@ -1,16 +1,34 @@
 import Promise from 'polyfill-promise';
-import { getLunch, getSlackNames } from './getSheets';
-import { getNextLunch, getDateColumn } from './getNextLunchDate';
-import { formatForGeneral, getUsers } from './parseNames';
-import { postToSlack } from './postToSlack';
+// import { getLunch, getSlackNames } from './getSheets';
+// import { getNextLunch, getDateColumn } from './getNextLunchDate';
+// import { formatForGeneral, getUsers } from './parseNames';
+import { initChannels, postToSlack, createMessage, retrieveUserMap } from './postToSlack';
 import * as dotenv from "dotenv";
 import * as _ from 'lodash';
 
 dotenv.config();
 
-const slackNames = ['@zacknotzach', '<@kamran>', '<@jacob>'];
+const slackNames = ['zacknotzach', 'kamran', 'jacob', 'KarenCam Angelo', 'igor'];
 
-// const msg = 'Hello to ' + _.sample(slackNames);
-const msg = `hello to <@zacknotzach>`;
-postToSlack('#lunch-bot-test-channel', msg)
+const shuffle = (array) => {
+    array.sort(() => Math.random() - 0.5);
+}
+
+const parseGroups = (names) => {
+    let groups = [];
+    const maxSize = 5;
+    shuffle(names);
+    for (let i = 0; i < names.length; i += maxSize) {
+        let temp = names.slice(i, i + maxSize);
+        groups.push(temp);
+    }
+    console.log(groups);
+    return groups;
+}
+
+retrieveUserMap()
+.then((userMap) => {
+    initChannels(parseGroups(slackNames), userMap);
+});
+
 
--- src/postToSlack.js
+++ src/postToSlack.js
@@ -1,22 +1,12 @@
 const { WebClient } = require('@slack/web-api');
 import * as _ from 'lodash';
 
-// const postToSlack = (channel, msg) => {
-//   const slack = new Slack.WebClient(process.env.slackToken);
-
-//   slack.chat.postMessage(channel, msg, {
-//     unfurl_links: true,
-//     as_user: true,
-//   });
-// };
-
 const retrieveUserMap = () =>
   new Promise((resolve, reject) => {
     const slack = new WebClient(process.env.slackToken);
     slack.users.list()
     .then(users => {
       let members = users.members.filter(m => !m.deleted && !m.is_bot);
-      console.log(members.length);
       members = members.map(m => {m.displayname = m.profile.display_name; return m;});
       resolve(members);
     });
@@ -30,25 +20,51 @@ const getUserIDsFromNames = (users, group) => {
 
   return group.map(u => {
     let result = users.find(user => user.name === u) || users.find(user => user.displayname === u);
-    console.log(result);
     return result ? result.id : '';
   });
 };
 
-const createChannels = (group) => {
-  // groups.forEach(element => {
-    
-  // });
-  const slack = new WebClient(); 
-  slack.conversations.create({
-    token: {token: process.env.slackToken},
-    name: 'test-channel-name',
-    is_private: true,
-    user_ids: mapUsers(group)
+const postToSlack = (channel, msg) => {
+  const slack = new WebClient(process.env.slackToken);
+  slack.chat.postMessage({
+    token: process.env.slackToken,
+    channel: channel,
+    text: msg
+  }).then(mess => {
+    console.log(msg, 'posted');
+  });
+};
+
+const createMessage = (group) => {
+  const start = "It’s time to plan your Lunch Match! The group is "; 
+  const names = group.join(', ');
+  const base = ". \nSome things to consider: \n- Set up a time \n- Decide what to eat \n- Check if anyone has dietary restrictions \n- In the office or out of the office?";
+  return start + names + base;
+};
+
+// const 
+
+const initChannels = (groups, userMap) => {
+  const slack = new WebClient(process.env.slackToken); 
+  groups.forEach(group => {
+    let chanName = `${Math.floor(Math.random() * 10000)}-test-channel`;
+    let users = getUserIDsFromNames(userMap, group).join(',');
+    console.log(chanName, ' with ', users);
+    slack.conversations.create({
+      token: process.env.slackToken,
+      name: chanName,
+      is_private: true,
+      user_ids: users
+    }).then(x => {
+      const channel = x.channel.id;
+      console.log(channel);
+      addUsers(channel, users);
+      postToSlack('#'+chanName, createMessage(group));
+    });
   });
 }
 
 
-export { createChannels };
 export { getUserIDsFromNames };
 export { retrieveUserMap };
+export { initChannels, postToSlack, createMessage };
