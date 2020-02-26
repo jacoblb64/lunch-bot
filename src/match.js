fe2514e78e32a07c339fda5372be0d47c0d16cdb
Zack Shinkar <zack@havenlife.com>
2020-02-26 14:20:56 -0500

    Remove optOuts from optIns in parseGroups

--- src/match.js
+++ src/match.js
@@ -18,16 +18,20 @@ const shuffle = (array) => {
 //   console.log(value);
 // });
 
-const parseGroups = (names) => {
+const parseGroups = (optIns, optOuts) => {
     let groups = [];
     const maxSize = 4;
-    names.filter((a, b) => name.indexOf(a) === b); // filter duplicates
-    shuffle(names);
-    for (let i = 0; i < names.length; i += maxSize) {
+    // filter duplicates and remove optOuts
+    optIns.filter((a, b) => name.indexOf(a) === b); 
+    optOuts.filter((a, b) => name.indexOf(a) === b);
+    optIns.filter(user => !optOuts.includes(user));
+
+    shuffle(optIns);
+    for (let i = 0; i < optIns.length; i += maxSize) {
         let temp = names.slice(i, i + maxSize);
         groups.push(temp);
     }
-    if (names.length % maxSize === 1) {
+    if (optIns.length % maxSize === 1) {
       groups[groups.length - 1].push(groups[0][0]);
       groups[0] = groups[0].slice(1, maxSize);
     }
@@ -41,7 +45,7 @@ retrieveUserMap()
 .then((userMap) => {
   console.log(userMap);
   // getGoogleSheetUsers.then((slackNames)=>{
-  //   initChannels(parseGroups(slackNames), userMap);
-  initChannels(parseGroups(slackEmails), userMap);
+  //   initChannels(parseGroups(optInEmails, optOutEmails), userMap);
+  initChannels(parseGroups(optInEmails, optOutEmails), userMap);
   // });
 });
